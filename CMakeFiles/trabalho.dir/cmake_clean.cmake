ee090eb35fb3d78e40ceda513e5b60761d4658d8
forestileao <pedroforestileao@gmail.com>
2021-08-21 02:19:36 -0300

    first commit

---
+++ CMakeFiles/trabalho.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho.dir/main.c.o"
+  "CMakeFiles/trabalho.dir/mazeSolver.c.o"
+  "CMakeFiles/trabalho.dir/stack.c.o"
+  "trabalho"
+  "trabalho.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trabalho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
